e806fdc020b558cfedbc4f0ba8690b954c43355c
siso9to <yonepin@gmail.com>
2025-01-19 03:07:30 +0900

    modify build reflextion error

--- lib/rays/extension.rb
+++ lib/rays/extension.rb
@@ -1,35 +1,37 @@
-module Rays
+require_relative "native"
+
+#module Rays
 
 
-  module Extension
+#  module Extension
 
-    module_function
+#    module_function
 
-    def name()
-      super.split('::')[-2]
-    end
-
-    def version()
-      File.read(root_dir 'VERSION')[/[\d\.]+/]
-    end
-
-    def root_dir(path = '')
-      File.expand_path "../../#{path}", __dir__
-    end
-
-    def inc_dir()
-      root_dir 'include'
-    end
-
-    def lib_dir()
-      root_dir 'lib'
-    end
-
-    def ext_dir()
-      root_dir 'ext'
-    end
-
-  end# Extension
-
-
-end# Rays
+#    def name()
+#      super.split('::')[-2]
+#    end
+#
+#    def version()
+#      File.read(root_dir 'VERSION')[/[\d\.]+/]
+#    end
+#
+#    def root_dir(path = '')
+#      File.expand_path "../../#{path}", __dir__
+#    end
+#
+#    def inc_dir()
+#      root_dir 'include'
+#    end
+#
+#    def lib_dir()
+#      root_dir 'lib'
+#    end
+#
+#    def ext_dir()
+#      root_dir 'ext'
+#    end
+#
+#  end# Extension
+#
+#
+#end# Rays
